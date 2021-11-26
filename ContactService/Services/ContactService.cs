fa33e9283ff480d6460bd33d93b8887028588b26
Nicolas Wazulek <nicolas.wazulek@niwadev.com>
2021-11-26 19:30:29 +0100

    chore: added .editorconfig

---
+++ ContactService/Services/ContactService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ContactService.Services
+{
+    public class ContactService
+    {
+        public ContactService()
+        {
+        }
+    }
+}
---
+++ ContactService/Services/IConactService.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace ContactService
+{
+    public interface IConactService
+    {
+    }
+}
